325bb4fdefd30258cf913878790e48544b7cf347
alwayswinners <kyehyuk.ahn@blockchainos.org>
2018-07-13 10:00:01 +0900

    show node information

--- docker-compose-config/get_database.sh
+++ docker-compose-config/get_database.sh
@@ -2,8 +2,9 @@
 field=$1
 mkdir $field
 for i in {1..12}
-do 
+do
 	docker_id=$(docker ps -a | grep "db"$i"_1" | cut -d ' ' -f 1)
+	echo $i" :"
 	docker exec -i $docker_id psql -U postgres -d stellar -c "\copy (select * from "$field") to "$field".csv with csv"
 	docker cp $docker_id:ledgerheaders.csv $field/"node"$i"_ledgerheaders.csv"
 done
